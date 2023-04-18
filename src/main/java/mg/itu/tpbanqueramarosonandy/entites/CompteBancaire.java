854e01f4cad8765cbdabd95055481603e7816371
AndyFaniry <ramarosonandyfaniry@gmail.com>
2023-04-18 19:34:10 +0300

    TP5: Modifiez l'entité CompteBancaire

--- src/main/java/mg/itu/tpbanqueramarosonandy/entites/CompteBancaire.java
+++ src/main/java/mg/itu/tpbanqueramarosonandy/entites/CompteBancaire.java
@@ -114,10 +114,12 @@ public class CompteBancaire implements Serializable {
     public CompteBancaire(String nom, int solde) {
         this.nom = nom;
         this.solde = solde;
+        this.operations.add(new OperationBancaire("Création du compte", solde));
     }
 
     public void deposer(int montant) {
         solde += montant;
+        this.operations.add(new OperationBancaire("Crédit", montant));
     }
 
     public void retirer(int montant) {
@@ -126,5 +128,6 @@ public class CompteBancaire implements Serializable {
         } else {
             solde = 0;
         }
+        this.operations.add(new OperationBancaire("Dédit", (-montant)));
     }
 }
